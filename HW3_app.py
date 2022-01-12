a6bb717db8bdabf811e549ed1e533ba9dc9bda40
mcs275 <mcs275@macbook-air.lan>
2022-01-12 11:30:01 -0500

    updating code

--- HW3_app.py
+++ HW3_app.py
@@ -10,9 +10,9 @@ st.title("Predicting Bikeshare Rentals")
 url = r"https://raw.githubusercontent.com/mcs275/dat-11-15-streamlit/main/bikeshare.csv"
 
 num_rows = st.sidebar.number_input('Select Number of Rows to Load', 
-                                   min_value = 1000, 
+                                   min_value = 100, 
                                    max_value = 50000, 
-                                   step = 1000)
+                                   step = 100)
 
 section = st.sidebar.radio('Choose Application Section', ['Data Explorer', 
                                                           'Model Explorer'])
@@ -52,6 +52,7 @@ if section == 'Data Explorer':
     elif chart_type == 'bar':
         grouping = create_grouping(x_axis, y_axis)
         st.bar_chart(grouping)
+        
     elif chart_type == 'area':
         fig = px.strip(df[[x_axis, y_axis]], x=x_axis, y=y_axis)
         st.plotly_chart(fig)
@@ -63,31 +64,67 @@ else:
     model = load_model()
     
     
-  #  days_val = st.sidebar.number_input("Enter Number of Days since start of data", 
-   ##                              min_value = 0, max_value = 100, step = 1, value = 20),
+    days_val = st.sidebar.number_input("Enter Number of Days since start of data", 
+                                min_value = 0, max_value = 10000, step = 1, value = 20)
     
-  ##  time_val = st.sidebar.selectbox("Choose Time of Day", 
-      ##                          df['time_of_day'].unique().tolist() )
+    time_val = st.sidebar.selectbox("Choose Time of Day", 
+                               df['time_of_day'].unique().tolist())
     
- ##   dayofweek_val = st.sidebar.selectbox("Choose Day of Week", 
-     ##                            df['day_of_week'].unique().tolist() ), 
+    dayofweek_val = st.sidebar.selectbox("Choose Day of Week", 
+                               df['day_of_week'].unique().tolist() )
 
     weather_val = st.sidebar.selectbox("Choose Weather", 
-                                  df['weather'].unique().tolist() ),
+                                  df['weather'].unique().tolist())
    
-    last_hour = st.sidebar.number_input("How many bike shares rented prior hour?", min_value = 0,
-                                    max_value = 500, step = 1, value = 20)
     
     season = st.sidebar.selectbox("season", 
                                        df['season'].unique().tolist())
     
+    holiday = st.sidebar.selectbox("Select if it's a holiday", 
+                                       df['holiday'].unique().tolist())
+    
+    workingday = st.sidebar.selectbox("Select if a working day", 
+                                       df['workingday'].unique().tolist())
+    
+    temp = st.sidebar.number_input("What is the temperature (celsius)?", min_value = 0,
+                                    max_value = 50, step = 0.5, value = 20)
+    
+    avg_temp = st.sidebar.number_input("What is the average day temperature?", min_value = 0,
+                                    max_value = 50, step = 0.5, value = 23)
+    
+    humidity = st.sidebar.number_input("What is the humidity?", min_value = 0,
+                                    max_value = 100, step = 5, value = 25)
+    
+    windspeed = st.sidebar.number_input("What is the wind speed?", min_value = 0,
+                                    max_value = 60, step = 5, value = 10)  
+    
+    last_hour = st.sidebar.number_input("How many bike rented prior hour?", min_value = 0,
+                                    max_value = 1500, step = 1, value = 191)
+    
+    yesterday = st.sidebar.number_input("How many bikes rented 24 hours ago?", min_value = 0,args=(), 
+                                         max_value = 1500, step = 1, value = 191)
+      
+    week_ago = st.sidebar.number_input("How many bikes rented 7 days ago", min_value = 0,
+                                    max_value = 1500, step = 1, value = 191)
+    
+    month_ago = st.sidebar.number_input("How many bikes rented 1 month ago?", min_value = 0,
+                                    max_value = 1500, step = 1, value = 191)
     sample = {
-  ## 'Days': days_val,
-   ##'time_of_day': time_val,
-  ## 'day_of_week': dayofweek_val,
-    'weather': weather_val,
-   '1hourago': last_hour,
-    'season': season
+        'Days': days_val,
+        'time_of_day': time_val,
+        'day_of_week': dayofweek_val,
+        'weather': weather_val,
+        '1hourago': last_hour,
+        'season': season,
+        'holiday': holiday,
+        'workingday': workingday,
+        'temp': temp,
+        'atemp': avg_temp,
+        'humidity': humidity,
+        'windspeed': windspeed,
+        '24hourago': yesterday,
+        '7dayago': week_ago,
+        '1monthago': month_ago
     }
 
     sample = pd.DataFrame(sample, index = [0])
