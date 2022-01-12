bdca302503902192e080f4bfe80b22e568670b54
mcs275 <mcs275@macbook-air.lan>
2022-01-12 12:59:47 -0500

    height

--- HW3_app.py
+++ HW3_app.py
@@ -47,7 +47,7 @@ if section == 'Data Explorer':
     
     if chart_type == 'line':
         grouping = create_grouping(x_axis, y_axis)
-        st.line_chart(grouping, height=100)
+        st.line_chart(grouping, height=1000)
         
     elif chart_type == 'bar':
         grouping = create_grouping(x_axis, y_axis)
